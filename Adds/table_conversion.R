8185ad9ede74b68013f36a837e33fdf00309afc2
Dae Ho Kim <modurang@gmail.com>
2019-05-03 09:55:35 +0900

    Table Conversion
    Long Table <-> Wide Table

--- Adds/table_conversion.R
+++ Adds/table_conversion.R
@@ -10,14 +10,14 @@ install.packages("tidyr")
 library(tidyr)
 
 #------------------------------------------------
-# read Data (table_conversion__01.csv : Long Table)
+# 1. read Data (table_conversion__01.csv : Long Table)
 #------------------------------------------------
 
 data_table <- read.csv(file.choose(), header=T)
 data_table
 
 #------------------------------------------------
-# Table Conversion : Long to Wide
+# 2. Table Conversion : Long to Wide
 #------------------------------------------------
 
 # key=항목, value=X2019..03.월
@@ -27,7 +27,7 @@ wide_table <- spread(data_table, 항목, X2019..03.월)
 wide_table
 
 #------------------------------------------------
-# Table Conversion : Wide to Long
+# 3. Table Conversion : Wide to Long
 #------------------------------------------------
 
 long_table <- gather(wide_table, '시도간전입[명]':'총전출[명]', key="항목", value="X2019..03.월")
@@ -35,7 +35,7 @@ long_table
 
 
 #------------------------------------------------
-# read Data (table_conversion__02.csv : Long Table)
+# 1. read Data (table_conversion__02.csv : Long Table)
 #------------------------------------------------
 
 data_table <- read.csv(file.choose(), header=T)
@@ -43,7 +43,7 @@ data_table
 
 
 #------------------------------------------------
-# Table Conversion : Long to Wide
+# 2. Table Conversion : Long to Wide
 #------------------------------------------------
 
 # key=항목, value=X2019..03.월
@@ -54,14 +54,14 @@ wide_table <- spread(data_table, 항목, X2019..03.월)
 wide_table
 
 #------------------------------------------------
-# Data Table 수정
+# *** Data Table 수정 ***
 #------------------------------------------------
 
 # 이 경우 data file의 '시도내이동-시군구내[명]'에 있는 중복값을 수정해야 한다.
 # 수정된 파일 : table_conversion_03.csv
 
 #------------------------------------------------
-# read Data (table_conversion__02.csv : Long Table)
+# 1. read Data (table_conversion__02.csv : Long Table)
 #------------------------------------------------
 
 data_table <- read.csv(file.choose(), header=T)
@@ -69,7 +69,7 @@ data_table
 
 
 #------------------------------------------------
-# Table Conversion : Long to Wide
+# 2. Table Conversion : Long to Wide
 #------------------------------------------------
 
 # key=항목, value=X2019..03.월
@@ -77,7 +77,7 @@ wide_table <- spread(data_table, 항목, X2019..03.월)
 wide_table
 
 #------------------------------------------------
-# Table Conversion : Wide to Long
+# 3. Table Conversion : Wide to Long
 #------------------------------------------------
 
 long_table <- gather(wide_table, '시도간전입[명]':'총전출[명]', key="항목", value="X2019..03.월")
