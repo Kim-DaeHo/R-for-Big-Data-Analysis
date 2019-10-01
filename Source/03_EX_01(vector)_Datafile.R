5b92bdc5d1f9ad2aaaa4a2493e70c153734a7d0e
Dae Ho Kim <modurang@gmail.com>
2019-10-01 11:25:56 +0900

    Add files via upload

--- Source/03_EX_01(vector)_Datafile.R
+++ Source/03_EX_01(vector)_Datafile.R
@@ -10,62 +10,60 @@ setwd("C:/temp")
 install.packages("readxl")
 library(readxl)
 
-Stud_ID_2 <- read_excel("test.xlsx", 
+Stud_ID_1 <- read_excel("test.xlsx", 
            col_name=TRUE,      # TRUE to use the first row as column names
            sheet=1)            # sheet = 1
-Stud_ID_2
+Stud_ID_1
 
-Name_2 <- read_excel("test.xlsx", 
+Name_1 <- read_excel("test.xlsx", 
         col_name=TRUE,      # TRUE to use the first row as column names
         sheet=2)            # sheet = 2
-Name_2
+Name_1
 
-Dept_2 <- read_excel("test.xlsx", 
+Dept_1 <- read_excel("test.xlsx", 
        col_name=TRUE,      # TRUE to use the first row as column names
        sheet=3)            # sheet = 3
-Dept_2
+Dept_1
 
 
-Mid_2 <- read_excel("test.xlsx", 
+Mid_1 <- read_excel("test.xlsx", 
        col_name=TRUE,      # TRUE to use the first row as column names
        sheet=4)            # sheet = 3
-Mid_2
+Mid_1
 
-Final_2 <- read_excel("test.xlsx", 
+Final_1 <- read_excel("test.xlsx", 
          col_name=TRUE,      # TRUE to use the first row as column names
          sheet=5)            # sheet = 1~7
-Final_2
+Final_1
 
-Attendance_2 <- read_excel("test.xlsx", 
+Attendance_1 <- read_excel("test.xlsx", 
               col_name=TRUE,      # TRUE to use the first row as column names
               sheet=6)            # sheet = 1~7
-Attendance_2
+Attendance_1
 
 
 #------------------------------
 # 2. 입력 데이터 확인
 #------------------------------
-Stud_ID_2
-Name_2
-Mid_2
-Final_2
-Attendance_2
+Stud_ID_1
+Name_1
+Dept_1
+Mid_1
+Final_1
+Attendance_1
 
 #------------------------------
 # 3. Data 처리 : 요약정보(Summary), 처리(합산 - 사칙연산) 등
 #------------------------------
 
-test_2 <- cbind(Mid_2, Final_2, Attendance_2)
-rownames(test_2) <- Name_2  # 각 행의 이름 부여...
-test_2
+summary(Mid_1)
+summary(Final_1)
+summary(Attendance_1)
 
-total_2 <- apply(test_2, MARGIN=1, sum)  # 각 행별 합계를 구한다.
-test_2 <- cbind(test_2, total_2)
-test_2
+total_1 <- Mid_1 + Final_1 + Attendance_1
+total_1
 
-sum_2 <- apply(test_2, MARGIN=2, sum)  # 각 열별 합계를 구한다.
-test_sum_2 <- rbind(test_2, sum)
-test_sum_2
+summary(Total_1)
 
 #------------------------------
 # 4. 결과의 저장
